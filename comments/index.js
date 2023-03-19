c3c7896d49fdeb470879d499e5c85fb3bb4a8098
rick huang <rickyhuang0824@gmail.com>
2023-03-18 21:10:39 -0700

    created simple comments services backend

---
+++ comments/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+const commentsbyPostId = {};
+
+app.use(bodyParser.json());
+app.get("/posts/:id/comments", (req, res) => {
+    res.send(commentsbyPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+    const content = req.body.content;
+    const comments = commentsbyPostId[req.params.id] || [];
+    const commentId = randomBytes(4).toString("hex");
+    comments.push({ commentId, content });
+    commentsbyPostId[req.params.id] = comments;
+    res.status(200).send(comments);
+});
+
+app.listen(4001, () => console.log("Listening on 40001"));
