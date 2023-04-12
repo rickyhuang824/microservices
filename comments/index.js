5b4506d3a02dafd5a3e83a1567cfa175fd88dab4
rick huang <rickyhuang0824@gmail.com>
2023-04-11 21:53:29 -0700

    Added Moderation service logic Updating Comment service logic to receive comment moderated event and emit comment updated event

--- comments/index.js
+++ comments/index.js
@@ -27,14 +27,35 @@ app.post("/posts/:id/comments", async (req, res) => {
             id: commentId,
             content: content,
             postId: req.params.id,
+            status: "pending",
         },
     });
 
     res.status(200).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
     console.log("Received Event:", req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === "CommentModerated") {
+        const { id, postId, status } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find((comment) => comment.id === id);
+        comment.status = status;
+
+        await axios.post("http://localhost:4005/events", {
+            type: "CommentUpdated",
+            data: {
+                id,
+                status,
+                postId,
+                content,
+            },
+        });
+    }
+
     res.send({});
 });
 
--- moderation/index.js
+++ moderation/index.js
@@ -6,7 +6,27 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post("/event", (req, res) => {});
+app.post("/event", async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === "CommentCreated") {
+        const status = data.content.includes("orange")
+            ? "rejected"
+            : "approved";
+
+        await axios.post("http://localhost:4005/events", {
+            type: "CommentModerated",
+            data: {
+                id: data.id,
+                content: data.content,
+                postId: data.postId,
+                status: status,
+            },
+        });
+    }
+
+    res.send({});
+});
 
 app.listen(4003, () => {
     console.log("listening to port 4003");
